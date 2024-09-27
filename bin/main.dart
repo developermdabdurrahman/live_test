ae11734c8eac3d01efaaa1976425f9e679253742
macbookair <developermdabdurrahman@gmail.com>
2024-09-27 23:17:37 +0600

    add

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,10 @@
 import 'car.dart';
 
 void main() {
-  // Create an instance of the Car class
+
   Car myCar = Car('Toyota', 'Corolla', 2015);
 
-  // Display car details
+
   print('Brand: ${myCar.brand}');
   print('Model: ${myCar.model}');
   print('Year: ${myCar.year}');
